d0624f1ea36b06b16b68c41a08571e126b86c8a9
Janmejay Sahoo <janmejay.redmi11@gmail.com>
2026-03-10 19:31:43 +0530

    Initial commit: PetCare Backend project

---
+++ src/main/java/com/springboot/petcarebackend/PetcareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.petcarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetcareBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/petcarebackend/PetcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.petcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
